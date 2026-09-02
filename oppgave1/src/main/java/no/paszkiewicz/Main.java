bf23f23dcf1c1326c7ce01e43989d5ebec5e2e96
Bartosz Paszkiewicz <bartosz@paszkiewicz.no>
2026-09-02 19:51:17 +0200

    Oppgave 1a-b

--- oppgave1/src/main/java/no/paszkiewicz/Main.java
+++ oppgave1/src/main/java/no/paszkiewicz/Main.java
@@ -3,7 +3,10 @@ package no.paszkiewicz;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.Collections;
+
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,9 +21,21 @@ public class Main {
         });
 
         System.out.println("Oppgave 1 - Sortert liste: " + list_sorted);
+
+        // Oppgave 1b - Input
+        int sum = beregn(12, 13, (a, b) -> a + b);
+        System.out.println(sum);
+
+        int largest = beregn(-5, 3, (a, b) -> Math.max(a, b));
+        System.out.println(largest);
+
+        int absolute = beregn(54, 45, (a, b) -> Math.abs(a - b));
+        System.out.println(absolute);
     }
 
-    public static int beregn(int a, int b) {
-        return a + b;
+    // Oppgave 1b
+    // --------------------------------------------------------------------------
+    public static int beregn(int a, int b, BiFunction<Integer,Integer,Integer> operasjon) {
+        return operasjon.apply(a, b);
     }
 }
