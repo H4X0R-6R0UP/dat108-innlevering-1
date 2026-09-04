dafee1686a970082f741714c818aaddeb41903ec
brasswhisper-hub <daniel.aarsand@gmail.com>
2026-09-04 11:56:53 +0200

    NÅ fiksa oppgave 3
    re: tittel

--- oppgave3/src/main/java/no/paszkiewicz/Main3.java
+++ oppgave3/src/main/java/no/paszkiewicz/Main3.java
@@ -80,60 +80,7 @@ public class Main3 {
             return "\n"+getFornavn()+" "+getEtternavn()+", "+getKjonn()+", "+getStilling()+", "+getAarslonn();
         }
     }
-
-    public static void lonnsoppgjor(List<Ansatt> ansatte, int tall) {
-        for (Ansatt ansatt : ansatte) {
-            ansatt.setAarslonn(tall);
-        }
-    }
-
-    public static List<String> filterEtternavn(List<Ansatt> ansatte) {
-
-        return ansatte.stream()
-                .map(Ansatt::getEtternavn)
-                .collect(Collectors.toList());
-    }
-
-    public static long antallKvinner(List<Ansatt> ansatte) {
-
-        return ansatte.stream()
-                .map(Ansatt::getKjonn)
-                .filter(kjonn -> kjonn.equals(Kjonn.KVINNE)).count();
-    }
-
-    public static void snittlonnKvinner(List<Ansatt> ansatte) {
-
-        OptionalDouble dameliste = ansatte.stream()
-                .filter(Ansatt -> Ansatt.getKjonn() == Kjonn.KVINNE)
-                .mapToDouble(Ansatt::getAarslonn)
-                .average();
-
-        System.out.print(dameliste);
-    }
-
-    public static void sjeflonn(List<Ansatt> ansatte) {
-
-        ansatte.stream()
-                .filter(ansatt -> "Sjef".equals(ansatt.getStilling()))
-                .forEach(ansatt -> ansatt.setAarslonn(ansatt.getAarslonn() * 1.07));
-    }
-
-    public static boolean lonnSjekk(List<Ansatt> ansatte) {
-
-        return ansatte.stream()
-                .anyMatch(ansatt -> ansatt.getAarslonn() > 800000);
-
-
-    }
-
-    public static Ansatt lavestLonn(List<Ansatt> ansatte) {
-
-        return ansatte.stream()
-                .min(Comparator.comparingDouble(Ansatt::getAarslonn))
-                .get();
-    }
-
-
+    
     public static void main(String[] args) {
 
         Ansatt a1 = new Ansatt("Jan", "Mayen", Kjonn.MANN, "Øy", 1000000);
@@ -143,13 +90,42 @@ public class Main3 {
 
         List<Ansatt> ansatte = List.of(a1, a2, a3, a4);
 
-        System.out.print(filterEtternavn(ansatte)+"\n");
-        System.out.print(antallKvinner(ansatte)+"\n");
-        snittlonnKvinner(ansatte);
-        sjeflonn(ansatte);
-        System.out.println("\n"+ansatte);
-        System.out.print(lonnSjekk(ansatte));
-        System.out.print(lavestLonn(ansatte));
+        List<String> etternavn = ansatte.stream()
+                .map(Ansatt::getEtternavn)
+                .toList();
+
+        System.out.print(etternavn+"\n");
+
+        long kvinner = ansatte.stream()
+                .map(Ansatt::getKjonn)
+                .filter(kjonn -> kjonn.equals(Kjonn.KVINNE)).count();
+
+        System.out.print(kvinner+"\n");
+
+        OptionalDouble snittlisteKvinner = ansatte.stream()
+                .filter(Ansatt -> Ansatt.getKjonn() == Kjonn.KVINNE)
+                .mapToDouble(Ansatt::getAarslonn)
+                .average();
+        System.out.print(snittlisteKvinner+"\n");
+
+        List<Ansatt> sjef = ansatte.stream()
+                .filter(ansatt -> "Sjef".equals(ansatt.getStilling()))
+                .peek(ansatt -> ansatt.setAarslonn(ansatt.getAarslonn() * 1.07))
+                .toList();
+
+        System.out.print(sjef+"\n");
+
+        boolean godtbetalt = ansatte.stream()
+                .anyMatch(ansatt -> ansatt.getAarslonn() > 800000);
+
+        System.out.print(godtbetalt+"\n");
+
+        System.out.println(ansatte+"\n");
+
+        Ansatt daarligBetalt = ansatte.stream()
+                .min(Comparator.comparingDouble(Ansatt::getAarslonn))
+                .get();
+        System.out.print(daarligBetalt+"\n");
 
         int delelig = IntStream.rangeClosed(1, 1000)
                 .filter(n -> n % 3 == 0 || n % 5 == 0)
